f9286d62217374ace59188d93975dd6dad5f0186
Uriah <codeitlikemiley@gmail.com>
2023-08-08 17:29:20 +0800

    added co-pilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = { markdown = true, help = true },
+  },
+}
