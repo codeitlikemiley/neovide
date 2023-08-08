0d09843c30297280191a4ba19468288701408e99
Uriah <codeitlikemiley@gmail.com>
2023-08-08 17:29:33 +0800

    telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- disable the keymap to grep files
+    { "<leader>/", false },
+    -- change a keymap
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    -- add a keymap to browse plugin files
+    {
+      "<D-p>",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
+    },
+  },
+}
