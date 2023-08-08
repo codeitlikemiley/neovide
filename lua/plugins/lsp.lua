0e26afa9dfceac6e32062980537008403755b195
Uriah <codeitlikemiley@gmail.com>
2023-08-08 22:07:32 +0800

    add CMD+R and f2 for rename keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,10 +8,16 @@ return -- LSP keymaps
     if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("gui_running") == 1 then
       keys[#keys + 1] =
         { "<D-.>", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v", "i" }, has = "codeAction" }
+
+      keys[#keys + 1] = { "<D-r>", vim.lsp.buf.rename, desc = "Rename", has = "rename" }
+      keys[#keys + 1] = { "<F2>", vim.lsp.buf.rename, desc = "Rename", has = "rename" }
     else
       keys[#keys + 1] =
         { "<C-.>", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v", "i" }, has = "codeAction" }
+
+      keys[#keys + 1] = { "<F2>", vim.lsp.buf.rename, desc = "Rename", has = "rename" }
     end
+
     -- end modifying lsp keymaps
   end,
   --  Modify other LSP  settings below this line
