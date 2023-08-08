621d12a2c52fc3c5b0567589c5eed25ac0b7f490
Uriah <codeitlikemiley@gmail.com>
2023-08-08 17:39:56 +0800

    update clipboard reference is neovide docs

--- lua/config/keymaps/clipboard.lua
+++ lua/config/keymaps/clipboard.lua
@@ -1,7 +1,12 @@
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
-  vim.cmd([[
-nnoremap <D-v> "+p
-inoremap <D-v> <c-r>+
-cnoremap <D-v> <c-r>+
-]])
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+else
+  vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 end
