9a162123d2704dc60d49527db7cc6f8f595e44da
Uriah <codeitlikemiley@gmail.com>
2023-08-08 20:41:27 +0800

    movelines with CMD j and k

--- lua/config/keymaps/movelines.lua
+++ lua/config/keymaps/movelines.lua
@@ -1,19 +1,11 @@
-local nnoremap = require("utils/nnoremap")
+local map = require("utils/map")
 
-local vnoremap = require("utils/vnoremap")
-
-local inoremap = require("utils/inoremap")
-
--- This is MACOS Specific Keybinding OPT+[jk]
-
-nnoremap("∆", [[ :m .+1<CR>==]])
-
-nnoremap("˚", [[ :m .-2<CR>==]])
-
-inoremap("∆", [[<Esc>:m .+1<CR>==gi]])
-
-inoremap("˚", [[<Esc>:m .-2<CR>==gi]])
-
-vnoremap("∆", [[ :m '>+1<CR>gv=gv]])
-
-vnoremap("˚", [[:m '<-2<CR>gv=gv]])
+-- Move Lines
+if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
+  map("n", "<D-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+  map("n", "<D-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+  map("i", "<D-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+  map("i", "<D-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+  map("v", "<D-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+  map("v", "<D-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+end
