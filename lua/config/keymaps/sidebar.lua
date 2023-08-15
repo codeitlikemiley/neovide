fefb89aab72ef677c1f0315952336fa6a137eb3c
Uriah <codeitlikemiley@gmail.com>
2023-08-16 02:30:25 +0800

    change to command `

--- lua/config/keymaps/sidebar.lua
+++ lua/config/keymaps/sidebar.lua
@@ -1,5 +1,5 @@
 local nnoremap = require("utils/nnoremap")
 
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("gui_running") == 1 then
-  nnoremap("<D-b>", "[[:Neotree action=show  toggle=true<CR>]]")
+  nnoremap("<D-`>", "[[:Neotree action=show  toggle=true<CR>]]")
 end
