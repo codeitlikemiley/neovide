63922f49be65986c9f6b28043b8d5d31c8edb8d3
Uriah <codeitlikemiley@gmail.com>
2023-08-16 02:30:47 +0800

    trim whitespaces

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- trim white spaces
+vim.cmd([[
+    autocmd BufWritePre * %s/\s\+$//e
+    autocmd BufWritepre * %s/\n\+\%$//e
+]])
