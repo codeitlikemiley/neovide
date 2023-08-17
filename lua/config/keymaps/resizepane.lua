798cf3c5acb60369f4387f4aa0fe275e1a40ab52
Uriah <codeitlikemiley@gmail.com>
2023-08-17 21:27:45 +0800

    type description

--- lua/config/keymaps/resizepane.lua
+++ lua/config/keymaps/resizepane.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "__", ":resize -5<CR>", { silent = true, desc = "Decrease Pane Horizontal"})
-vim.keymap.set("n", "++", ":resize +5<CR>", { silent = true, desc = "Increase Pane Horizontal"})
-vim.keymap.set("n", "--", ":vertical resize -5<CR>", { silent = true, desc = "Decrease Pane Vertical"})
-vim.keymap.set("n", "==", ":vertical resize +5<CR>", { silent = true, desc = "Increase Pane Vertical" })
+vim.keymap.set("n", "__", ":resize -5<CR>", { silent = true, desc = "Decrease Pane Horizontally"})
+vim.keymap.set("n", "++", ":resize +5<CR>", { silent = true, desc = "Increase Pane Horizontally" })
+vim.keymap.set("n", "--", ":vertical resize -5<CR>", { silent = true, desc = "Decrease Pane Vertically"})
+vim.keymap.set("n", "==", ":vertical resize +5<CR>", { silent = true, desc = "Increase Pane Vertically" })
