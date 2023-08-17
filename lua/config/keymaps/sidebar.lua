27be2900904bee42504270c1812399c77c7017a5
Uriah <codeitlikemiley@gmail.com>
2023-08-17 21:28:12 +0800

    add description

--- lua/config/keymaps/sidebar.lua
+++ lua/config/keymaps/sidebar.lua
@@ -1,3 +1,3 @@
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("gui_running") == 1 then
-  vim.keymap.set("n", "<D-b>", ":Neotree action=show toggle=true<CR>", { silent = true, desc = "Toggle Sidebar" })
+  vim.keymap.set("n", "<D-b>", ":Neotree action=show toggle=true position=left<CR>", { silent = true, desc = "Toggle Sidebar" })
 end
