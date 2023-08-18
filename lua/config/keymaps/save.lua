b84a175179807c1ef6ae9bfa61b5d4918cc5ffe7
Uriah <codeitlikemiley@gmail.com>
2023-08-18 15:27:13 +0800

    update save command

--- lua/config/keymaps/save.lua
+++ lua/config/keymaps/save.lua
@@ -1,6 +1,4 @@
 -- lazy.nvim already uses ctrl + s to save
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("gui_running") == 1 then
-  vim.keymap.set("n", "<D-s>", ":update<CR>", { silent = true, desc = "Save" })
-  vim.keymap.set("v", "<D-s>", "<C-C>:update<CR>", { silent = true, desc = "Save" })
-  vim.keymap.set("i", "<D-s>", "<C-O>:update<CR>", { silent = true, desc = "Save" })
+  vim.keymap.set({ "i", "v", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 end
