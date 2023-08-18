a71d6d30ec891b70b9cbc6108ead7106d884c4e4
Uriah <codeitlikemiley@gmail.com>
2023-08-18 15:27:06 +0800

    add blamer toggle

--- lua/config/keymaps/sidebar.lua
+++ lua/config/keymaps/sidebar.lua
@@ -1,3 +1,4 @@
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("gui_running") == 1 then
   vim.keymap.set("n", "<D-b>", ":Neotree action=show toggle=true position=left<CR>", { silent = true, desc = "Toggle Sidebar" })
+  vim.keymap.set("n", "<M-b>", ":BlamerToggle", { silent = true, desc = "Toggle Sidebar" })
 end
