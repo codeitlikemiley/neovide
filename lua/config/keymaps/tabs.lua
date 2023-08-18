d5f7374aa6dfe0ee004d11e31f2da5298a0d176f
Uriah <codeitlikemiley@gmail.com>
2023-08-18 12:09:30 +0800

    remove cmd + w, better use cmd + q or qq for smart quit

--- lua/config/keymaps/tabs.lua
+++ lua/config/keymaps/tabs.lua
@@ -9,5 +9,4 @@ if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("g
   vim.keymap.set("n", "<D-8>", "<Cmd>BufferLineGoToBuffer 8<CR>", { silent = true, desc = "Go to Tab 8" })
   vim.keymap.set("n", "<D-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", { silent = true, desc = "Go to Tab 9" })
   vim.keymap.set("n", "<D-n>", ":tabnew<cr>", { silent = true, desc = "New Tab" })
-  vim.keymap.set("n", "<D-w>", '<cmd>lua require("utils/smartquit")()<CR>', { silent = true, desc = "Smart Close Tab" })
 end
