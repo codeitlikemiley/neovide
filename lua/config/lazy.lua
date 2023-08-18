1e788068b0bb16dbc43a8d955814f9ca3c9373dd
Uriah <codeitlikemiley@gmail.com>
2023-08-18 20:20:53 +0800

    added vscode

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.vscode" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
