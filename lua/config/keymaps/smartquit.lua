c851f1feb0bbf57a9df57b9f1fcb399323be815e
Uriah <codeitlikemiley@gmail.com>
2023-08-18 12:34:10 +0800

    quit all on CMD + Q only if on mac

--- lua/config/keymaps/smartquit.lua
+++ lua/config/keymaps/smartquit.lua
@@ -1,1 +1,5 @@
 vim.keymap.set("n", "qq", '<cmd>lua require("utils/smartquit")()<CR>', { silent = true, desc = "Smart Quit" })
+
+if vim.fn.has('macunix') == 1 and vim.fn.exists('neovide') == 1 then
+    vim.keymap.set("n", "<D-q>", '<cmd>qa<cr>', { silent = true, desc = "Quit all" })
+end
