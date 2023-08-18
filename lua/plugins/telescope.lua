f7238d625d23b6ae7cff2a0e0d805ed37c3e75c1
Uriah <codeitlikemiley@gmail.com>
2023-08-18 12:10:40 +0800

    auto format code

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,6 @@ local function find_files_on_workspace()
   telescope_builtin.find_files({ cwd = cwd })
 end
 
-
 local function find_document_symbols()
   telescope_builtin.lsp_document_symbols({ cwd = cwd })
 end
@@ -31,7 +30,6 @@ local function find_dynamic_workspace_symbols()
   })
 end
 
-
 local function lazygit()
   Util.float_term({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
 end
@@ -40,10 +38,10 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     -- add a keymap to browse plugin files
-    { "<D-f>", Util.telescope("live_grep"),    desc = "Find Everyting on Workspace" },
-    { "<D-g>", lazygit,                        desc = "Open Lazy Git" },
-    { "<D-i>", find_document_symbols,          desc = "Go to Symbols on Current Open File" },
-    { "<D-o>", find_dynamic_workspace_symbols, desc = "Go to Symbol on Workspace", },
-    { "<D-p>", find_files_on_workspace,        desc = "Open Files on Current Working Directory" },
+    { "<D-f>", Util.telescope("live_grep"), desc = "Find Everyting on Workspace" },
+    { "<D-g>", lazygit, desc = "Open Lazy Git" },
+    { "<D-i>", find_document_symbols, desc = "Go to Symbols on Current Open File" },
+    { "<D-o>", find_dynamic_workspace_symbols, desc = "Go to Symbol on Workspace" },
+    { "<D-p>", find_files_on_workspace, desc = "Open Files on Current Working Directory" },
   },
 }
