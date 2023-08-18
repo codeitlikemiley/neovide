1cf335ccd14ed20b9199793b7cd58ce451d82ee8
Uriah <codeitlikemiley@gmail.com>
2023-08-18 12:11:17 +0800

    use command t to run debug test under cursor

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -7,21 +7,38 @@ return {
     },
     keys = function()
       if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 and vim.fn.has("gui_running") == 1 then
-        vim.keymap.set("n", "<D-e>", ":lua require(\"neotest\").run.run()<CR>", { silent = true, desc = "Run Test on Cursor" })
-        vim.keymap.set("n", "<D-x>", ":lua require(\"neotest\").run.run({strategy = \"dap\"})<CR>", { silent = true, desc = "Debug Test on Cursor" })
-        vim.keymap.set("n", "<D-/>", ":lua require(\"neotest\").summary.toggle()<CR>", { silent = true, desc = "Test Summary" })
+        vim.keymap.set(
+          "n",
+          "<D-e>",
+          ':lua require("neotest").run.run()<CR>',
+          { silent = true, desc = "Run Test on Cursor" }
+        )
+        vim.keymap.set(
+          "n",
+          "<D-t>",
+          ':lua require("neotest").run.run({strategy = "dap"})<CR>',
+          { silent = true, desc = "Debug Test on Cursor" }
+        )
       else
-        vim.keymap.set("n", "<M-e>", ":lua require(\"neotest\").run.run()<CR>", { silent = true, desc = "Run Test on Cursor" })
-        vim.keymap.set("n", "<M-x>", ":lua require(\"neotest\").run.run({strategy = \"dap\"})<CR>", { silent = true, desc = "Debug Test on Cursor" })
-        vim.keymap.set("n", "<M-/>", ":lua require(\"neotest\").summary.toggle()<CR>", { silent = true, desc = "Test Summary" })
+        vim.keymap.set(
+          "n",
+          "<M-e>",
+          ':lua require("neotest").run.run()<CR>',
+          { silent = true, desc = "Run Test on Cursor" }
+        )
+        vim.keymap.set(
+          "n",
+          "<M-t>",
+          ':lua require("neotest").run.run({strategy = "dap"})<CR>',
+          { silent = true, desc = "Debug Test on Cursor" }
+        )
       end
-
     end,
     opts = {
       adapters = {
         "neotest-plenary",
         ["neotest-rust"] = {},
-      }
+      },
     },
   },
 }
