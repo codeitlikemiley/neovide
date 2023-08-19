7b2594276707c698c06f8a5385f49348c0cd9222
Uriah <codeitlikemiley@gmail.com>
2023-08-19 16:29:38 +0800

    lazy load rust snippet

--- lua/plugins/snip.lua
+++ lua/plugins/snip.lua
@@ -3,14 +3,18 @@ return {
   build = (not jit.os:find("Windows"))
       and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
     or nil,
+  keys = function()
+    return {}
+  end,
   dependencies = {
     "rafamadriz/friendly-snippets",
     config = function()
-      require("luasnip.loaders.from_vscode").load({
+      require("luasnip.loaders.from_vscode").lazy_load({
+        -- remove all snippets
         include = { "rust" },
-        -- make sure to create this folder
+        -- load our snippets
         --paths = {
-        -- "~/.config/nvim/snippets"
+        --  "~/.config/nvim/snippets",
         --},
       })
     end,
