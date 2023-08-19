f05bffeac98d2bbd91ff7dc58d81dc6ca26e3198
Uriah <codeitlikemiley@gmail.com>
2023-08-19 16:48:28 +0800

    remove blur , has no effect on mac

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,9 +7,9 @@ if vim.g.neovide then
 
   vim.g.neovide_scroll_animation_length = 0.3
 
-  vim.g.neovide_hide_mouse_when_typing = false
+  vim.g.neovide_hide_mouse_when_typing = true
 
-  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_refresh_rate = 60
 
   vim.g.neovide_refresh_rate_idle = 5
 
@@ -19,11 +19,8 @@ if vim.g.neovide then
 
   vim.g.neovide_transparency = 0
   vim.g.transparency = 0.95
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
-  vim.g.neovide_floating_blur_amount_x = 15.0
-  vim.g.neovide_floating_blur_amount_y = 15.0
-  --vim.opt.guifont = "Operator Mono Lig:h16"
-  vim.opt.guifont = "Hack Nerd Font:h16"
+  vim.g.neovide_background_color = "#0f1119" .. alpha()
+  -- vim.opt.guifont = "Operator Mono Lig:h16"
 end
 
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
