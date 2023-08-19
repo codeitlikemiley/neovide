6959c1d610a35504363616bd900c3318dcf0ab93
Uriah <codeitlikemiley@gmail.com>
2023-08-19 12:42:57 +0800

    load only rust snippet, add comment to point to our custom snippets folder

--- lua/plugins/snip.lua
+++ lua/plugins/snip.lua
@@ -1,5 +1,20 @@
 return {
   "L3MON4D3/LuaSnip",
+  build = (not jit.os:find("Windows"))
+      and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+    or nil,
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").load({
+        include = { "rust" },
+        -- make sure to create this folder
+        --paths = {
+        -- "~/.config/nvim/snippets"
+        --},
+      })
+    end,
+  },
   opts = {
     history = true,
     delete_check_events = "TextChanged",
