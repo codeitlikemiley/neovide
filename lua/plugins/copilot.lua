f4790bc1f44d31b9b61e35a6998fcbcc136dc524
Uriah <codeitlikemiley@gmail.com>
2023-08-27 19:24:09 +0800

    fix tabbing issue with copilot and snippets

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,10 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
   opts = {
     suggestion = { enabled = false },
     panel = { enabled = false },
-    filetypes = { markdown = true, help = true },
   },
 }
