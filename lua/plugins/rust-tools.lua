946bf9315a02260c85ccacf97c035e0bff623d6a
Uriah <codeitlikemiley@gmail.com>
2023-08-28 19:05:01 +0800

    autoformat code

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -12,7 +12,7 @@ return {
       local extension_path = codelldb:get_install_path() .. "/extension/"
       local codelldb_path = extension_path .. "adapter/codelldb"
       local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-          or extension_path .. "lldb/lib/liblldb.so"
+        or extension_path .. "lldb/lib/liblldb.so"
       adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
     end
     return {
