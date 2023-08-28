969eb821effebdcfd0758ff672bb6b0c1210ada8
Uriah <codeitlikemiley@gmail.com>
2023-08-28 19:17:31 +0800

    disable default keymap of cmd + r

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,4 @@
 local cargo_run = require("utils/cargo_run")
-
 return -- LSP keymaps
 {
   "neovim/nvim-lspconfig",
@@ -10,8 +9,9 @@ return -- LSP keymaps
     if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 or vim.fn.has("gui_running") == 1 then
       keys[#keys + 1] =
         { "<D-.>", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v", "i" }, has = "codeAction" }
-
-      keys[#keys + 1] = { "<D-r>", ":cargo_run()<CR>", desc = "Cargo Run", mode = { "n", "v", "i" } }
+      -- disable default keymap of CMD + r
+      keys[#keys + 1] = { "<D-r>", false }
+      keys[#keys + 1] = { "<D-r>", ":lua cargo_run()<CR>", desc = "Cargo Run", mode = { "n", "v", "i" } }
       keys[#keys + 1] = { "<F2>", vim.lsp.buf.rename, desc = "Rename", mode = { "n", "v", "i" }, has = "rename" }
     else
       keys[#keys + 1] =
