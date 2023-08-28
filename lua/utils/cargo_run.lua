ba5db1376409676b86fcee45ceb65dc91206f972
Uriah <codeitlikemiley@gmail.com>
2023-08-28 20:42:59 +0800

    use fnmemodify instead of a cwd:match for getting filename  and foldername

--- lua/utils/cargo_run.lua
+++ lua/utils/cargo_run.lua
@@ -1,20 +1,23 @@
 function cargo_run()
-  -- get local current working directory
+  -- Get local current working directory
   local cwd = vim.fn.getenv("NEOVIDE_CWD") or vim.fn.getcwd()
-  local folder_name = cwd:match("^.+/(.+)$")
+  local folder_name = vim.fn.fnamemodify(cwd, ":t")
 
   -- Get the file name without extension to be used in --bin
   local filename = vim.fn.expand("%:r")
-  filename = filename:match("^.+/(.+)$")
+  filename = vim.fn.fnamemodify(filename, ":t")
 
-  -- if main.rs then filename = folder_name
+  -- If main.rs, then filename = folder_name
   if filename == "main" then
     filename = folder_name
   end
 
-  -- Construct the cargo test command
+  -- Construct the cargo run command
   local cmd = "RUSTFLAGS='-A warnings' cargo run -p " .. folder_name .. " --bin " .. filename
 
-  vim.cmd("! " .. cmd)
+  vim.cmd(":new")
+  -- Run the command in the terminal
+  vim.cmd(":term " .. cmd)
 end
+
 return cargo_run
