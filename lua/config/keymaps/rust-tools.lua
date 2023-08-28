5aafc326f1ecbe45d228b72358c888937ead4a0a
Uriah <codeitlikemiley@gmail.com>
2023-08-28 19:05:17 +0800

    remove cmd + r

--- lua/config/keymaps/rust-tools.lua
+++ lua/config/keymaps/rust-tools.lua
@@ -13,13 +13,11 @@ vim.g.rust_inlay_hints_enabled = true
 local custom_attach = function(_, bufnr)
   if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
     -- MacOS Keymaps
-    vim.keymap.set("n", "<D-r>", ":RustRun<CR>", { silent = true, desc = "Rust Run App" })
     vim.keymap.set("n", "<D-i>", ":lua RustToggleInlayHints()<CR>", { silent = true, desc = "Rust Toggle Inlay Hints" })
     vim.keymap.set("n", "<D-m>", ":RustExpandMacro<CR>", { silent = true, desc = "Expand Rust Macro" })
   elseif
-      vim.fn.has("unix") == 1 and vim.fn.exists("neovide") == 1
-      or vim.fn.has("win32")
-      and vim.fn.exists("neovide") == 1
+    vim.fn.has("unix") == 1 and vim.fn.exists("neovide") == 1
+    or vim.fn.has("win32") and vim.fn.exists("neovide") == 1
   then
     -- Windows and Linux Keymaps
     vim.keymap.set("n", "<M-r>", ":RustRun<CR>", { silent = true, desc = "Rust Run App" })
@@ -48,6 +46,7 @@ local custom_attach = function(_, bufnr)
     { silent = true, desc = "Test Summary" }
   )
   vim.keymap.set("n", "<leader>rd", ":RustDebuggables<CR>", { silent = true, desc = "Rust Debuggables" })
+  vim.keymap.set("n", "<leader>rn", ":!cargo run<CR>", { silent = true, desc = "Cargo Run" })
   vim.keymap.set("n", "<leader>rm", ":RustExpandMacro<CR>", { silent = true, desc = "Expand Rust Macro" })
   vim.keymap.set("n", "<leader>rH", ":RustEnableInlayHints<CR>", { silent = true, desc = "Show Rust Inlay Hint" })
   vim.keymap.set("n", "<leader>rh", ":RustDisableInlayHints<CR>", { silent = true, desc = "Disable Rust Inlay Hint" })
