050bf4524229585080991cc9a2e60eca03395af3
Uriah <codeitlikemiley@gmail.com>
2023-12-30 12:21:14 +0800

    direct setup of lldb / faster than default

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -3,43 +3,32 @@ return {
   "simrat39/rust-tools.nvim",
   dependencies = { "mattn/webapi-vim" },
   lazy = true,
-  opts = function()
-    local ok, mason_registry = pcall(require, "mason-registry")
-    local adapter ---@type any
-    if ok then
-      -- rust tools configuration for debugging support
-      local codelldb = mason_registry.get_package("codelldb")
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-          or extension_path .. "lldb/lib/liblldb.so"
-      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-    end
-    return {
-      server = {
-        -- all keybindings related to rust is attached to this function
-        on_attach = on_attach,
-        -- standalone file support
-        -- setting it to false may improve startup time
-        standalone = false,
+  opts = {
+    server = {
+      -- all keybindings related to rust is attached to this function
+      on_attach = on_attach,
+      -- standalone file support
+      -- setting it to false may improve startup time
+      standalone = false,
+    },
+    dap = {
+      adapter = {
+        type = "executable",
+        command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
+        name = "rt_lldb",
       },
-      dap = {
-        adapter = adapter,
-      },
-      tools = {
-        reload_workspace_from_cargo_toml = true,
-        on_initialized = function()
-          vim.cmd([[
-                 augroup RustLSP
-                   autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-                   autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-                   autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-                 augroup END
-               ]])
-        end,
-      },
-    }
-  end,
-  config = function()
-  end,
+    },
+    tools = {
+      reload_workspace_from_cargo_toml = true,
+      on_initialized = function()
+        vim.cmd([[
+               augroup RustLSP
+                 autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                 autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                 autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+               augroup END
+             ]])
+      end,
+    },
+  },
 }
