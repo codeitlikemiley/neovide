058b4d93d26243bfc75beda92f30f9349290033f
Uriah <codeitlikemiley@gmail.com>
2024-01-08 15:06:20 +0800

    added indent

---
+++ lua/config/keymaps/indent.lua
@@ -0,0 +1,3 @@
+
+vim.keymap.set( "v", "<", "<gv", {  desc = "Visual mode Indent Decrease",  silent = true })
+vim.keymap.set( "v", ">", ">gv", {  desc = "Visual mode Indent Increase",  silent = true })
