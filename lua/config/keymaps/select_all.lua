9ce0b5721662260535e7177d8869ca9f2b369284
Uriah <codeitlikemiley@gmail.com>
2024-01-08 15:06:51 +0800

    added shift l and h to allow us to quickly move start of line or eol

--- lua/config/keymaps/select_all.lua
+++ lua/config/keymaps/select_all.lua
@@ -12,3 +12,11 @@ vim.api.nvim_set_keymap("c", "<D-a>", "<Esc>ggVG", { noremap = true, silent = tr
 
 -- Terminal Mode: Escape to Normal Mode, then Select All
 vim.api.nvim_set_keymap("t", "<D-a>", "<C-\\><C-n>ggVG", { noremap = true, silent = true })
+
+
+
+local modes = { 'n', 'o', 'x' }
+for _, mode in ipairs(modes) do
+    vim.api.nvim_set_keymap(mode, '<S-h>', '^', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(mode, '<S-l>', 'g_', { noremap = true, silent = true })
+end
