cdda93231786818d343bd1612e6e87e000f94bb3
Uriah <codeitlikemiley@gmail.com>
2024-07-25 23:08:48 +0800

    update keymaps related to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,4 @@
 local telescope_builtin = require("telescope.builtin")
-local Util = require("lazyvim.util")
 -- if nil cwd then get env var called NEOVIDE_CWD
 local cwd = vim.fn.getcwd()
 
@@ -45,22 +44,24 @@ local function find_dynamic_workspace_symbols()
   })
 end
 
-local function lazygit()
-  Util.float_term({ "lazygit" }, { esc_esc = false, ctrl_hjkl = false })
-end
 
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
-    { "<D-F3>",    "<cmd>TroubleToggle document_diagnostics<cr>",        desc = "Document Diagnostics (Trouble)" },
-    { "<M-p>",     "<cmd>Telescope diagnostics<cr>",                     desc = "Workspace diagnostics" },
-    { "<D-f>",     Util.telescope("live_grep"),                          desc = "Find Everyting on Workspace" },
-    { "<D-g>",     lazygit,                                              desc = "Open Lazy Git" },
-    { "<M-d>",     ":DiffviewFileHistory %<CR>",                         desc = "Diff File History" },
-    { "<D-o>",     find_document_symbols,                                desc = "Go to Symbols on Current Open File" },
-    { "<D-t>",     find_dynamic_workspace_symbols,                       desc = "Go to Workspace Symbols" },
-    { "<D-p>",     find_files_on_workspace,                              desc = "Open Files on Current Working Directory" },
-    { "<D-e>",     Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)",                           mode = { "n", "v", "i" } },
-    { "<S-space>", "<cmd>Telescope keymaps<cr>",                         desc = "Key Maps" },
+    -- lua LazyVim.pick("oldfiles")()
+    { "<D-e>",  ":lua LazyVim.pick('oldfiles')()<CR>",  desc = "Open Recent Files" },
+    { "<D-f3>", ":DiffviewFileHistory %<CR>",           desc = "Diff File History" },
+    {
+      "<D-f4>",
+      function()
+        vim.cmd('DiffviewClose')
+        vim.cmd('bdelete')
+      end,
+      desc = "Diff View Close"
+    },
+    { "<D-o>",     find_document_symbols,          desc = "Go to Symbols on Current Open File" },
+    { "<D-t>",     find_dynamic_workspace_symbols, desc = "Go to Workspace Symbols" },
+    { "<D-p>",     find_files_on_workspace,        desc = "Open Files on Current Working Directory" },
+    { "<S-space>", "<cmd>Telescope keymaps<cr>",   desc = "Key Maps" },
   },
 }
