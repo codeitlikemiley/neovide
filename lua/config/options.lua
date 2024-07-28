052e985565d783607527d257e5b47c8dea7e483a
Uriah <codeitlikemiley@gmail.com>
2024-07-28 17:13:31 +0800

    macos blur

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,10 +19,22 @@ if vim.g.neovide then
 
   vim.g.neovide_input_macos_alt_is_meta = true
 
-  vim.g.neovide_transparency = 0
-  vim.g.transparency = 0.95
-  vim.g.neovide_background_color = "#0f1119" .. alpha()
-  -- vim.opt.guifont = "Operator Mono Lig:h16"
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_window_blurred = true;
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_show_border = true
+  vim.g.neovide_scroll_animation_length = 0.3
+  vim.g.neovide_theme = 'auto'
+  vim.opt.guifont = "Operator Mono Lig:h14"
 end
 
 if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
