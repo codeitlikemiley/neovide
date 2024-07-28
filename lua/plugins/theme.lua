6693bacef44a0066c862f3a663e57a860da6728a
Uriah <codeitlikemiley@gmail.com>
2024-07-28 17:13:40 +0800

    better syntax highlighting

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -8,7 +8,16 @@ return {
         floats = "transparent",
       },
       overrides = {
-        ["@lsp.type.enum"] = { fg = "#b681ef" },
+        ["@lsp.type.interface"] = { fg = "#b681ef" },
+        ["@lsp.type.enum"] = { fg = "#F694C1" },
+        ["@lsp.type.enumMember"] = { fg = "#00A5CF" },
+        ["@lsp.type.variable"] = { fg = "#89D2DC" },
+        ["@lsp.type.property"] = { fg = "#705D56" },
+        ["@string"] = { fg = "#ebdbb2" },
+        ["@lsp.type.method"] = { fg = "#F78154" },
+        ["@lsp.type.function"] = { fg = "#F78154" },
+        ["Comment"] = { fg = "#705D56", italic = true },
+        --Comment       xxx cterm=italic gui=italic guifg=#928374
       },
     },
   },
